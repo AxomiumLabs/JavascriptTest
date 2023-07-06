0dbd0545249ad664abed0ddf766ef820801db7c8
ANAINAP <anainarajeev3@gmail.com>
2023-07-06 12:53:53 +0530

    javascript task 1 to 6

---
+++ anaina/nodejs/5_array.js
@@ -0,0 +1,13 @@
+let fruits=["mango","orange","apple"]
+// console.log(fruits[0]);
+// fruits.push("kiwi")
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+fruits.forEach((result)=>{console.log(result)})
+let status=fruits.includes("mangos")
+console.log(status);
+
+console.log(fruits)
+fruits.reverse()
+console.log(fruits)
